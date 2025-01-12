7cf58f5e36e959e34d96426cbaa008f518b28907
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-12 14:02:14 -0300

    feature: criação da instanciação do banco utilizando docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { DirectorsModule } from './directors/directors.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MoviesModule, DirectorsModule, DatabaseModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), MoviesModule, DirectorsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
