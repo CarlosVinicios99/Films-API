31a5d2db24e951307eb90a1829ec22443f964fbb
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-12 13:36:57 -0300

    feature: criação inicial do módulo de diretores

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { DirectorsModule } from './directors/directors.module';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [MoviesModule, DirectorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/directors/directors.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from "@nestjs/common";
+
+@Controller('directors')
+export class DirectorsController {
+
+}
