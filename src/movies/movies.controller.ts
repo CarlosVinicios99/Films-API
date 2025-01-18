c0412b70a02eec5f3fc4196ae7807aeb22329845
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-18 14:22:38 -0300

    feature: criação de rota para criar filme

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,14 +1,17 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { CreateMovieDto } from "./dto/CreateMovieDto";
+import { MoviesService } from "./movies.service";
 
 @Controller('movies')
 export class MoviesController {
 
-  constructor(){}
+  constructor(
+    private readonly moviesService: MoviesService
+  ){}
     
   @Post()
   create(@Body() body: CreateMovieDto){
-    
+    return this.moviesService.createMovie(body)
   }
 
 }
